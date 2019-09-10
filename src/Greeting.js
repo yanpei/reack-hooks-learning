b2e31b138e2c04b67df2c27320a6c31ccf7fc092
yanpei <pyan@thoughtworks.com>
2019-09-10 23:09:44 +0800

    use state of class component to change name

--- src/Greeting.js
+++ src/Greeting.js
@@ -2,14 +2,27 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 
-function Greeting(props){
-  return (
-    <section>
-      <Row label="Name"> 
-        {props.name}
-      </Row>
-    </section>
-  );
+class Greeting extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'Marry',
+    };
+  }
+
+  handleChangeName = (e) => {
+    this.setState({name: e.target.value});
+  }
+
+  render(){
+    return  (
+      <section>
+        <Row label="Name">
+          <input value={this.state.name} onChange={this.handleChangeName}></input> 
+        </Row>
+      </section>
+    );
+  }
 }
 
 export default Greeting;
