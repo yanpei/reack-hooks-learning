bb06350fbc7f77c2c9f0d6a9a6300669ad961c80
yanpei <pyan@thoughtworks.com>
2019-09-10 23:24:25 +0800

    use state hooks method multiple times in one component

--- src/Greeting.js
+++ src/Greeting.js
@@ -4,15 +4,23 @@ import Row from './Row';
 
 function Greeting(props){
   const [name, setName] = useState('Marry');
+  const [surname, setSurname] = useState('Gilbert');
   
-  function handleChangeName(e){
+  function handleNameChange(e){
     setName(e.target.value);
+  }
+
+  function handleSurnameChange(e){
+    setSurname(e.target.value);
   }
 
   return (
     <section>
       <Row label="Name"> 
-        <input value={name} onChange={handleChangeName}></input>
+        <input value={name} onChange={handleNameChange}></input>
+      </Row>
+      <Row label="Surname"> 
+        <input value={surname} onChange={handleSurnameChange}></input>
       </Row>
     </section>
   );
