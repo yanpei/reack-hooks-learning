1ddee62fbb26982870927458d973c30f63083154
yanpei <pyan@thoughtworks.com>
2019-09-11 00:07:07 +0800

    use effect hooks multiple times, and return a function to clean up effect

--- src/Greeting.js
+++ src/Greeting.js
@@ -10,6 +10,16 @@ function Greeting(props){
     document.title = name + ' ' + surname;
   });
 
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  })
+
   function handleNameChange(e){
     setName(e.target.value);
   }
@@ -25,6 +35,9 @@ function Greeting(props){
       </Row>
       <Row label="Surname"> 
         <input value={surname} onChange={handleSurnameChange}></input>
+      </Row>
+      <Row label="Width"> 
+        {width}
       </Row>
     </section>
   );
