408d8a4b532c05d453d627c09047760b05ffeba0
yanpei <pyan@thoughtworks.com>
2019-09-10 23:48:13 +0800

    use effect hooks

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Row from './Row';
 
@@ -6,6 +6,10 @@ function Greeting(props){
   const [name, setName] = useState('Marry');
   const [surname, setSurname] = useState('Gilbert');
   
+  useEffect(() => {
+    document.title = name + ' ' + surname;
+  });
+
   function handleNameChange(e){
     setName(e.target.value);
   }
